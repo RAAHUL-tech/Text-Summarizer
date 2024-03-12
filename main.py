fd6357f2d5583586a4fbe43b717515b184166c09
RAAHUL-tech <rahulkrish28@gmail.com>
2024-03-12 18:57:28 +0530

    Data_ingestion pipline updated

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from Summarizer.pipeline.data_injestion_1 import DataIngestionTrainingPipeline
 from Summarizer.logging import logger
 
-logger.info("Testing our log file")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
